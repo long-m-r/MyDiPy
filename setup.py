213e55c45b87094b7b6889499cba2797ff293251
Matt Long <long.matt.r@gmail.com>
2020-12-14 11:39:29 -0600

    update setup.py for pypi release

--- setup.py
+++ setup.py
@@ -6,8 +6,10 @@ if sys.version_info < (3,5):
 setup(
     name='MyDiPy',
     version='0.0.1',
-    description='Runtime-enforced type checking, method/function overloading, multiple dispatch, inheritance, and casting for Python 3',
+    description='Function overloading, type checking, multiple dispatch, inheritance, and casting in Python 3.5+',
+    long_description='Runtime-enforced type checking, method/function overloading, multiple dispatch, inheritance, and casting for Python 3.5+. Uses MyPy/PEP 484-style annotations but where it actually is enforced at runtime.',
     author='Matthew Long',
+    url='https://github.com/long-m-r/MyDiPy',
     # author_email='null@null.com',
     license='MIT',
     packages=['mydipy'],
